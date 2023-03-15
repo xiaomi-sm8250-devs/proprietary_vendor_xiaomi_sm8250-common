29a2051c1162a4b0a19ff0af6b2ca6782d60176b
Sebastiano Barezzi <seba@sebaubuntu.dev>
2023-03-15 23:39:13 +0100

    sm8250-common: Add dagu and psyche to the build guards
    Change-Id: I726f1aba47b8661e36c131681fd518f1ec100647

--- Android.mk
+++ Android.mk
@@ -4,6 +4,6 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter alioth apollon cas cmi elish enuma lmi munch thyme umi,$(TARGET_DEVICE)),)
+ifneq ($(filter alioth apollon cas cmi dagu elish enuma lmi munch psyche thyme umi,$(TARGET_DEVICE)),)
 
 endif
